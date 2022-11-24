20e8bd5737c1dee8321025705fa7e2470c2bfb00
Denis Rebrov <denisrebrof@gmail.com>
2022-11-25 00:20:59 +0800

    Initial commit

---
+++ src/test/kotlin/com/denisrebrof/sringboottest/SringBootTestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.denisrebrof.sringboottest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SringBootTestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
