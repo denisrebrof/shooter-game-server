b4f0859e5f8642349e4dff35f3f38e99492407f0
Denis Rebrov <denisrebrof@gmail.com>
2022-12-03 12:30:54 +0800

    added cofiguration

--- src/main/kotlin/com/denisrebrof/sringboottest/messaging/components/ChatMessageConfiguration.kt
+++ src/main/kotlin/com/denisrebrof/sringboottest/messaging/components/ChatMessageConfiguration.kt
@@ -3,10 +3,10 @@ package com.denisrebrof.sringboottest.messaging.components
 import com.denisrebrof.sringboottest.messaging.ChatMessage
 import com.denisrebrof.sringboottest.messaging.MessageType
 import org.springframework.context.annotation.Bean
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Configuration
 
-@Component
-class ChatMessageComponent {
+@Configuration
+class ChatMessageConfiguration {
     @Bean
     fun getChatMessageBean(): ChatMessage = ChatMessage(
         messageType = MessageType.CONNECT,
