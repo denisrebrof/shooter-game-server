6f1451c2e4723e83f7b44e0b5c3de547b738db08
Denis Rebrov <denisrebrof@gmail.com>
2022-12-05 00:44:49 +0800

    added web app setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
+    war
 }
 
 group = "com.denisrebrof"
@@ -23,13 +24,17 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     runtimeOnly("com.h2database:h2")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    implementation("org.thymeleaf:thymeleaf-spring5")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/denisrebrof/sringboottest/AppConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.denisrebrof.sringboottest
-
-import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
-
-@Configuration
-class AppConfig {
-}
