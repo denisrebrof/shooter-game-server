4b1c9949d54f644b397e3fcf6d35d2087ad19134
Denis Rebrov <denisrebrof@gmail.com>
2023-04-14 22:56:13 +0800

    removed ttt-related code

--- src/main/kotlin/com/denisrebrof/springboottest/tictac/gateways/model/TicTacCellUpdateResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.denisrebrof.springboottest.tictac.gateways.model
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class TicTacCellUpdateResponse(
-    val cellPos: Int,
-    val isMine: Boolean
-)
